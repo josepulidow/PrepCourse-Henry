20396410fadde4569610789c845d1c7fd8b85f4d
josepulidow <josepulidow@gmail.com>
2023-01-30 23:18:09 -0600

    Este es el primer archivo en el cual se empieza como tal a ver JS

---
+++ variables.js
@@ -0,0 +1,19 @@
+// String
+var bootcamp = "Henry";
+
+
+// Números
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+// Boolenaos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// undefined
+var cajon1;
+console.log(cajon1);
+
+// Null
+var sinNada = null;
